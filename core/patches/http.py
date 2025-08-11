475208c8386a5813654208b507c81128e4235241
vEXOULZ <vexoulz@gmail.com>
2025-08-11 18:06:07 -0300

    HEEEEEEEEEEEEEEELP

--- core/patches/http.py
+++ core/patches/http.py
@@ -27,7 +27,7 @@ class TwitchHTTPWrapper(TwitchHTTP):
         self._refresh_token = auths.refresh_token # WHY: not exactly sure why I can't create a bot with a refresh token
 
 
-    @wraps(TwitchHTTP._generate_login)
+    # @wraps(TwitchHTTP._generate_login)
     async def _generate_login(self):
         redirecter = await super()._generate_login()
         # self.auths.client_id     = self.client_id # NOTE should never change
@@ -38,28 +38,28 @@ class TwitchHTTPWrapper(TwitchHTTP):
         self.client._connection._token = self.token # TODO: do this in a nicer way
         return redirecter
     
-    # WHY didnt it check for closed connections??
-    # trying to keep the same connection alive for days is wild
-    def check_session(self):
-        if   not self.session    : logging.info("No session found: new aiotthp client session created")
-        # elif self.session.closed : logging.info("Session closed: new aiotthp ClientSession created")
-        else                     : return
-        self.session = aiohttp.ClientSession()
+    # # WHY didnt it check for closed connections??
+    # # trying to keep the same connection alive for days is wild
+    # def check_session(self):
+    #     if   not self.session    : logging.info("No session found: new aiotthp client session created")
+    #     # elif self.session.closed : logging.info("Session closed: new aiotthp ClientSession created")
+    #     else                     : return
+    #     self.session = aiohttp.ClientSession()
+
+    # # WHY didnt it check for closed connections??
+    # @wraps(TwitchHTTP.request)
+    # async def request(self, *args, **kwargs):
+    #     # self.check_session()
+    #     return await super().request(*args, **kwargs)
+
+    # # WHY didnt it check for closed connections??
+    # @wraps(TwitchHTTP._generate_login)
+    # async def _generate_login(self, *args, **kwargs):
+    #     # self.check_session()
+    #     return await super()._generate_login(*args, **kwargs)
 
     # WHY didnt it check for closed connections??
-    @wraps(TwitchHTTP.request)
-    async def request(self, *args, **kwargs):
-        # self.check_session()
-        return await super().request(*args, **kwargs)
-
-    # WHY didnt it check for closed connections??
-    @wraps(TwitchHTTP._generate_login)
-    async def _generate_login(self, *args, **kwargs):
-        # self.check_session()
-        return await super()._generate_login(*args, **kwargs)
-
-    # WHY didnt it check for closed connections??
-    @wraps(TwitchHTTP.validate)
+    # @wraps(TwitchHTTP.validate)
     async def validate(self, *, token: str = None) -> dict:
         # self.check_session()
         try:
